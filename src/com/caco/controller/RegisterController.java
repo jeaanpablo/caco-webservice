e1ba22dd39a69dfe0c92cc6644410195c45963b6
prisrocken <prisrocken@hotmail.com>
2015-09-27 17:29:55 -0300

    Register Modifications

--- src/com/caco/controller/RegisterController.java
+++ src/com/caco/controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.caco.controller;
 import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.object.StoredProcedure;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.caco.facade.AddressFacade;
 import com.caco.facade.ClientFacade;
 import com.caco.facade.EmployeeFacade;
+import com.caco.facade.StoresFacade;
 import com.caco.facade.UserFacade;
 import com.caco.model.Address;
 import com.caco.model.Clients;
 import com.caco.model.Contact;
 import com.caco.model.Employees;
+import com.caco.model.Stores;
 import com.caco.model.Users;
 
 @RestController
@@ -35,9 +38,12 @@ public class RegisterController {
 	@Autowired
 	EmployeeFacade empFacade;
 	
+	@Autowired 
+	StoresFacade storeFacade;
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/registerEmployee")
-	public @ResponseBody Boolean sendMessage(
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/registerStore")
+	public @ResponseBody Boolean registerStore(
 			@RequestParam(value = "name") String name,
 			@RequestParam(value = "last_name") String last_name,
 			@RequestParam(value = "cpf") long cpf,
@@ -56,7 +62,113 @@ public class RegisterController {
 			@RequestParam(value = "country") String country,
 			@RequestParam(value = "login") String login,
 			@RequestParam(value = "password") String password,
-			@RequestParam(value = "email") String email)
+			@RequestParam(value = "email") String email,
+			@RequestParam(value = "cnpj") long cnpj,
+			@RequestParam(value = "description") String description,
+			@RequestParam(value = "fantasyname") String fantasy)
+			
+
+	{		
+		
+		Users userVerifyEmail = userFacade.findByEmail(email);
+		
+		if (userVerifyEmail != null) {
+			
+			
+            Stores store = new Stores();
+			
+			store.setCnpj(cnpj);
+			store.setDescription(description);
+			store.setFantasyName(fantasy);	
+			
+			Stores storeInserted = storeFacade.insert(store);
+			
+			Address address = new Address();
+			
+			address.setStreet(street);
+			address.setNumber(number);
+			address.setDistrict(district);
+			address.setCity(city);
+			address.setState(state);
+			address.setZipCode(zip_code);
+			address.setCountry(country);
+			
+			Address addressInserted = addressFacade.insert(address);
+			
+			
+		
+		}
+			
+		else {	
+			
+			
+		Users user = new Users();
+			
+		user.setLogin(login);
+		user.setEmail(email);
+		user.setPassword(password);	
+			
+		Users userInserted = userFacade.insert(user);	
+		
+		Clients register = new Clients();		
+		
+		register.setName(name);	
+		register.setLastName(last_name);
+		register.setCpf(cpf);
+		register.setbDate(b_data);
+		register.setGender(gender);
+		register.setRg(rg);		
+		
+		Clients clientInserted = clientFacade.insert(register);
+		
+		
+		
+		Address address = new Address();
+		
+		address.setStreet(street);
+		address.setNumber(number);
+		address.setDistrict(district);
+		address.setCity(city);
+		address.setState(state);
+		address.setZipCode(zip_code);
+		address.setCountry(country);
+		
+		Address addressInserted = addressFacade.insert(address);
+	
+		}
+		
+		return null;
+
+	}
+	
+	
+	
+	//
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/registerEmployee")
+	public @ResponseBody Boolean registerEmployee(
+			@RequestParam(value = "name") String name,
+			@RequestParam(value = "last_name") String last_name,
+			@RequestParam(value = "cpf") long cpf,
+			@RequestParam(value = "b_data") Calendar b_data,
+			@RequestParam(value = "gender") String gender,
+			@RequestParam(value = "rg") String rg,
+			@RequestParam(value = "salary") String salary,
+			@RequestParam(value = "hire_date") Calendar hire_date,
+			@RequestParam(value = "doc") long doc,
+			@RequestParam(value = "street") String street,
+			@RequestParam(value = "number") long number,
+			@RequestParam(value = "district") String district,
+			@RequestParam(value = "city") String city,
+			@RequestParam(value = "state") String state,
+			@RequestParam(value = "zip_code") long zip_code,
+			@RequestParam(value = "country") String country,
+			@RequestParam(value = "login") String login,
+			@RequestParam(value = "password") String password,
+			@RequestParam(value = "email") String email,
+			@RequestParam(value = "cnpj") long cnpj,
+			@RequestParam(value = "description") String description,
+			@RequestParam(value = "fantasyname") String fantasy)
 			
 
 	{		
@@ -102,7 +214,7 @@ public class RegisterController {
 	
 		
 		
-		return false;
+		return null;
 
 	}
 
